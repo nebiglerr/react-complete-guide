e60e8cb56a8bb4f06cde4bef0499c76afe53a293
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-10-27 21:46:45 +0300

    starting-project
    JSX & React Components , Creating & Using a First Custom Component,A Closer Look: Components & File Extensions,How React Handles Components & How It Builds A "Component Tree" , Prop Syntaxes, Making Components Reusable with Props , Setting HTML Attributes Dynamically & Loading Image Files, Using & Outputting Dynamic Values

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,15 +1,56 @@
+import reactImg from '../src/assets/react-core-concepts.png';
+import componentsImg from '../src/assets/components.png';
+import {CORE_CONCEPTS} from '../src/data.js'
+
+
+
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+function CoreConcept({image,title,description}) {
+  return (
+    <li>
+      <img src={image} alt='' />
+      <h3> {title}</h3>
+      <p>{description}</p>
+    </li>
+
+  );
+}
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+  return (<header>
+    <img src={reactImg} alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      {description} React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>)
+}
+
+
 function App() {
+
+  const coreConceptItems = [];
+  for (let i = 0; i < CORE_CONCEPTS.length; i++) {
+    coreConceptItems.push(<CoreConcept {...CORE_CONCEPTS[i]} />)
+  }
+
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+          {coreConceptItems}
+          </ul>
+
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
