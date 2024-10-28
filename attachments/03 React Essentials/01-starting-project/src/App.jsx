e7cbe0f1d30454b9590a6829592d1ce8fc191a9d
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-10-28 18:56:06 +0300

    2.part
     Best Practice: Storing Components in Files & Using a Good Project Structure,
     Storing Component Style Files Next To Components,
    Component Composition: The special "children" Prop ,
    Reacting to Events , Passing Functions as Values to Props,
    Passing Functions as Values to Props

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,36 +1,9 @@
-import reactImg from '../src/assets/react-core-concepts.png';
 import componentsImg from '../src/assets/components.png';
-import {CORE_CONCEPTS} from '../src/data.js'
+import { CORE_CONCEPTS } from '../src/data.js'
+import Header from './Components/Header/Header.jsx'
+import CoreConcept from './Components/CoreConcept.jsx'
+import TabButton from './Components/TabButton.jsx'
 
-
-
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-function CoreConcept({image,title,description}) {
-  return (
-    <li>
-      <img src={image} alt='' />
-      <h3> {title}</h3>
-      <p>{description}</p>
-    </li>
-
-  );
-}
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (<header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {description} React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>)
-}
 
 
 function App() {
@@ -40,6 +13,9 @@ function App() {
     coreConceptItems.push(<CoreConcept {...CORE_CONCEPTS[i]} />)
   }
 
+  function handleSelect(selectedButton) {
+    console.log(selectedButton)
+}
   return (
     <div>
       <Header />
@@ -47,8 +23,18 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-          {coreConceptItems}
+            {coreConceptItems}
           </ul>
+
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
 
         </section>
         <h2>Time to get started!</h2>
