8697e5490a303e41215a89da95d9e2bd0b04986b
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-10-28 22:17:29 +0300

    Deriving & Outputting Data Based on State

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { CORE_CONCEPTS } from '../src/data.js'
+import { EXAMPLES } from '../src/data.js'
 import Header from './Components/Header/Header.jsx'
 import CoreConcept from './Components/CoreConcept.jsx'
 import TabButton from './Components/TabButton.jsx'
@@ -7,7 +8,7 @@ import TabButton from './Components/TabButton.jsx'
 
 
 function App() {
-  const [selectedTopic,setSelectedTopic] = useState('Plase select tab');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   let tabContent = 'Plase select tab.'
 
@@ -40,7 +41,16 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+
+          </div>
         </section>
         <h2>Time to get started!</h2>
       </main>
