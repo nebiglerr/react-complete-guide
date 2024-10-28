73a7959c6aca9219d6667a5eefd19919904ad7df
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-10-28 23:07:54 +0300

    two different way array to map

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -11,17 +11,6 @@ function App() {
   const [selectedTopic, setSelectedTopic] = useState('');
 
   let tabContent = 'Plase select tab.'
-
-  const coreConceptItems = [];
-  const tabButtonItems = [];
-  for (let i = 0; i < CORE_CONCEPTS.length; i++) {
-    coreConceptItems.push(<CoreConcept {...CORE_CONCEPTS[i]} />)
-  }
-
-  for (let i = 0; i < CORE_CONCEPTS.length; i++) {
-    tabButtonItems.push(<TabButton isSelected={selectedTopic === CORE_CONCEPTS[i].title.toLowerCase()} onSelect={() => handleSelect(CORE_CONCEPTS[i].title.toLowerCase())}>{CORE_CONCEPTS[i].title}</TabButton>)
-  }
-
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     console.log(selectedTopic)
@@ -34,16 +23,16 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {coreConceptItems}
+            {CORE_CONCEPTS.map((conceptItem) => (<CoreConcept key={conceptItem.title} {...conceptItem} />))}
           </ul>
 
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-           {tabButtonItems}
+            {CORE_CONCEPTS.map((conceptItem) => (<TabButton key={conceptItem.title} isSelected={selectedTopic === conceptItem.title.toLowerCase()} onSelect={() => handleSelect(conceptItem.title.toLowerCase())}>{conceptItem.title}</TabButton>))}
           </menu>
-          {!selectedTopic && <p>Please select a topic.</p> }
+          {!selectedTopic && <p>Please select a topic.</p>}
           {selectedTopic && (<div id='tab-content'>
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
