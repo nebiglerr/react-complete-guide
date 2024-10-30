a749a7bacea5d887dcbfc4c248b6840064d9b91e
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-10-30 14:15:34 +0300

    Concept Repetition: Splitting Components & Building Reusable Components

--- attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
+++ attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
@@ -1,3 +1,4 @@
+import Player from "./component/Player"
 
 function App() {
 
@@ -8,14 +9,9 @@ function App() {
 
 
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">0</span>
-          </li>
+       
+       <Player name="Player 1" symbol="X"/>
+       <Player name="Player 2" symbol="O"/>
         </ol>
       </div>
       GAME BOARD
