faf241008152375f42fc68f0d6783e52008f010e
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-10-30 16:08:35 +0300

    Conditional Content & A Suboptimal Way Of Updating State

--- attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/component/Player.jsx
+++ attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/component/Player.jsx
@@ -4,12 +4,14 @@ export default function Player({ name, symbol }) {
     const [isEditing, setIsEditing] = useState(false)
 
     function handleEditClick() {
-        setIsEditing(true);
+        setIsEditing(!isEditing);
     }
 
     let playerName = (<span className="player-name">{name}</span>);
+    let btnCaption = 'Edit';
     if (isEditing) {
-        playerName = <input type="text" required  />
+        playerName = <input type="text" required value={name} />
+        btnCaption = 'Save';
     }
 
     return (<li>
@@ -17,6 +19,6 @@ export default function Player({ name, symbol }) {
             {playerName}
             <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleEditClick}>{btnCaption}</button>
     </li>);
 }
