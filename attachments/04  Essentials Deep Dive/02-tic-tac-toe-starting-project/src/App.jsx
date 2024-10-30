fc09cdffca582b80b782a6368602dc39a483189b
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-10-30 12:58:20 +0300

    New Project: First Steps Towards Our Tic-Tac-Toe Game

--- attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
+++ attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,25 @@
 
 function App() {
-  
+
 
   return (
-    <></>
+    <main>
+      <div id="game-container">
+
+
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">0</span>
+          </li>
+        </ol>
+      </div>
+      GAME BOARD
+    </main>
   )
 }
 
