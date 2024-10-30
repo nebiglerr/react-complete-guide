56f5194c5b9fed0df328565025987e1d75e8692b
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-10-30 17:57:48 +0300

    Rendering Multi-Dimensional Lists

--- attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
+++ attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
@@ -1,20 +1,16 @@
+import GameBoard from "./component/gAMEbOARD.JSX"
 import Player from "./component/Player"
-
 function App() {
-
-
   return (
     <main>
       <div id="game-container">
-
-
         <ol id="players">
-       
-       <Player initialName="Player 1" symbol="X"/>
-       <Player initialName="Player 2" symbol="O"/>
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
+      <GameBoard/>
+
       </div>
-      GAME BOARD
     </main>
   )
 }
