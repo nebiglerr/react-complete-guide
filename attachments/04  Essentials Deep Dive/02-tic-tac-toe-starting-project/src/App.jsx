fa2963142ac70ff40c7d5d3275c87aadc66a24e8
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-11-03 19:42:21 +0300

    Outsourcing Data Into A Separate File And Lifting Computed Values Up

--- attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
+++ attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
@@ -1,38 +1,57 @@
-import { useState } from "react"
-import GameBoard from "./component/gAMEbOARD.JSX"
-import Player from "./component/Player"
+import { useState } from "react";
+import GameBoard from "./component/GameBoard";
+import Player from "./component/Player";
 import Log from "./component/Log";
+import { WINNING_COMBINATIONS } from "./WINNING_COMBINATIONS";
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
 
-function deriveActivePlayer(gameTurns){
-
-  let currentPlayer = 'X';
-
-  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-    currentPlayer = 'O';
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
   }
   return currentPlayer;
 }
 
-
 function App() {
-
-  const [gameTurns, setGameTurns] = useState([])
+  const [gameTurns, setGameTurns] = useState([]);
 
   //const [activePlayer, setActivePlayer] = useState('X');
   const activePlayer = deriveActivePlayer(gameTurns);
-  
+
+  let gameBoard = initialGameBoard;
+
+  for (let turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+
+  for (let combination of WINNING_COMBINATIONS) {
+   
+    const firstSquareSymbol
+    const secondSquareSymbol
+    const SquareSymbol
+  }
+
+
 
   function handleSelectSquare(rowIndex, colIndex) {
-
-
-    setGameTurns(prevTurns => {
-      
+    setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
-   
-      const updateTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
 
+      const updateTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      console.log(updateTurns);
       return updateTurns;
     });
   }
@@ -40,16 +59,22 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare}
-        turns={gameTurns} />
-
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/component/GameBoard.jsx
+++ attachments/04  Essentials Deep Dive/02-tic-tac-toe-starting-project/src/component/GameBoard.jsx
@@ -1,37 +1,9 @@
 
-const initialGameBoard = [
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
-]
-export default function GameBoard({ onSelectSquare, turns }) {
 
-    let gameBoard = initialGameBoard;
-
-    for (let turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-        gameBoard[row][col] = player;
-    }
-
-    /*   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-   
-   
-       function handleSelectSquare(rowIndex, colIndex) {
-           setGameBoard((prevGameBoard) => {
-               const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-               updateBoard[rowIndex][colIndex] = activePlayerSymbol;
-               return updateBoard;
-           })
-           onSelectSquare();
-       }*/
+export default function GameBoard({ onSelectSquare, board }) {
 
     return (<ol id="game-board">
-        {gameBoard.map((row, rowIndex) =>
+        {board.map((row, rowIndex) =>
             <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) =>
