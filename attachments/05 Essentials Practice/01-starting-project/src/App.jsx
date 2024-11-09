d1e050f8cb28a8bb2e9f0c25779a4471e09de04f
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-11-09 11:27:55 +0300

    Lifting State Up

--- attachments/05 Essentials Practice/01-starting-project/src/App.jsx
+++ attachments/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import Header from "./component/Header.jsx";
 import UserInput from "./component/UserInput.jsx";
+import Result from "./component/Result.jsx";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 100000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue ) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput}  onChange={handleChange}/>
+      <Result input={userInput}/>
     </>
   );
 }
