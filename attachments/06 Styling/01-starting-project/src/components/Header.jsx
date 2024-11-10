c010bdf45414d4f5c1c64540dec0759dd0fca277
Nebi Güler <61344330+nebiglerr@users.noreply.github.com>
2024-11-10 16:58:18 +0300

    Dynamic & Conditional Inline Styles

--- attachments/06 Styling/01-starting-project/src/components/Header.jsx
+++ attachments/06 Styling/01-starting-project/src/components/Header.jsx
@@ -1,18 +1,11 @@
-import logo from "../assets/logo.png";
-import "./Header.css";
+import logo from '../assets/logo.png';
+import './Header.css'
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p
-        style={{
-          color: "red",
-          textAlign: "center",
-        }}
-      >
-        A community of artists and art-lovers.
-      </p>
+      <p>A community of artists and art-lovers.</p>
     </header>
   );
 }
